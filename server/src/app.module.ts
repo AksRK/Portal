bffd9d41bd0748f5bdd668fc5e15c1403d872dc0
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-04-10 17:57:20 +0300

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import {ConfigModule} from "@nestjs/config";
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({ isGlobal: true }),
+      MongooseModule.forRoot(process.env.DATABASE_URL),
+      UsersModule,
+      AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
