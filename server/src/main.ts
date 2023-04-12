85ba876df3ae130a7ece5a76f068457e818e07b9
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-04-12 21:33:06 +0300

    включена поддержка куков и корса

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import * as process from "process";
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({credentials:true, origin: process.env.CLIENT_URL});
+
   await app.listen(4444);
 }
 bootstrap();
