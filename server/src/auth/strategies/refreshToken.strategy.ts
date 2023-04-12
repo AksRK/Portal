872b9b97feeb5f52965c998cf978ed5b8b93059b
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-04-12 21:29:22 +0300

    рефреш через куки

--- server/src/auth/strategies/refreshToken.strategy.ts
+++ server/src/auth/strategies/refreshToken.strategy.ts
@@ -10,14 +10,20 @@ export class RefreshTokenStrategy extends PassportStrategy(
 ) {
 	constructor() {
 		super({
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			jwtFromRequest:ExtractJwt.fromExtractors([(request:Request) => {
+				let data = request?.cookies["refreshToken"];
+				if(!data){
+					return null;
+				}
+				return data
+			}]),
 			secretOrKey: process.env.JWT_REFRESH_SECRET,
 			passReqToCallback: true,
 		});
 	}
 
 	validate(req: Request, payload: any) {
-		const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+		const refreshToken = req.cookies["refreshToken"]
 		return { ...payload, refreshToken };
 	}
 }
