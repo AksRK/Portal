b901b9488f2fd8a325e8754def2903f68be291ce
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-04-12 21:34:20 +0300

    Добавлен глобальный стейт менеджер mob-x настроена проверка на авторизацию при загрузке страницы

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,33 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Store from "@/store/store";
+import {createContext, useContext, useEffect} from "react";
+
+interface State {
+    store: Store
+}
+
+const store = new Store()
+
+export const Context = createContext<State>({
+    store,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const {store} = useContext(Context)
+
+    useEffect(()=> {
+        if (localStorage.getItem('accessToken')) {
+            store.checkAuth()
+        }
+    }, [])
+
+
+
+  return (
+      <Context.Provider value={{store}}>
+          <Component {...pageProps} />
+      </Context.Provider>
+
+  )
 }
