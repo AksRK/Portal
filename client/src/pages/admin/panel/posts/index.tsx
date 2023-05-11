44037e9ce51bab30b5186b5f6715be58e3f04a91
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-12 01:19:24 +0300

    Сделана страница отображения таблицы с постами + управление (черновой вариант)

--- client/src/pages/admin/panel/posts/index.tsx
+++ client/src/pages/admin/panel/posts/index.tsx
@@ -1,12 +1,45 @@
-import React, {FC, useState} from 'react';
-import Editor from "@/components/Editor";
+import React, {FC, useEffect, useState} from 'react';
+import Link from "next/link";
+import {IPostData} from "@/core/types";
+import PostService from "@/services/post.service";
+import PostsTable from "@/components/Admin/Tables/Posts";
+import {Alert} from "@/core/utils/alert.utils";
 
 const AdminPosts:FC = () => {
-	const [editorState, setEditorState] = useState<string>('')
+
+	const [posts, setPosts] = useState<IPostData[] | []>([])
+
+	useEffect(() => {
+		getAllPost()
+	}, [])
+	const getAllPost = async () => {
+		await PostService.getALl('', '', '', '99')
+			.then((response) => {
+				if (response.data) {
+					setPosts(response.data.docs.flat())
+				}
+			}).catch((e) => {
+				console.log(e)
+			})
+	}
+
+	const removePost = async (id:string) => {
+		await PostService.remove(id)
+			.then((response) => {
+				getAllPost()
+				Alert({ msg: 'Пост удален', type: 'info' });
+			})
+			.catch((error) => {
+				Alert({ msg: error.response?.data?.message, type: 'error' });
+			})
+	}
 
 	return (
 		<>
-			<Editor initialContent={editorState} onChange={(data) => setEditorState(data)}/>
+			<Link href={'posts/create'}>
+				СОЗДАТЬ ПОСТ
+			</Link>
+			<PostsTable posts={posts} onDelete={removePost}/>
 		</>
 	);
 };
