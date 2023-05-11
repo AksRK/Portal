9a23047cea86d5b5055fcecbe715ed816f433d80
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-12 01:17:59 +0300

    Тестовые правки

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,18 @@
 import Head from 'next/head'
-import { useRef } from "react";
+import {FC} from "react";
 import CategoryNav from "@/components/CategoryNav";
-import ShrinkText from "@/components/UI/ShrinkText";
 import {API_URL} from "@/core/constants";
+import {IHomePageProps, IPostData} from "@/core/types";
+import Link from "next/link";
 
-export default function Home({categories}:any) {
+const Home:FC<IHomePageProps> = ({categories, posts}) => {
 
-    // const ttt = ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam aspernatur consequatur dolores eius eos excepturi ipsam ipsum magnam, officia quia quibusdam quidem. Cum deserunt fugit minima sequi, similique ut.\n' +
-    //     '                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam aspernatur consequatur dolores eius eos excepturi ipsam ipsum magnam, officia quia quibusdam quidem. Cum deserunt fugit minima sequi, similique ut.\n' +
-    //     '                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam aspernatur consequatur dolores eius eos excepturi ipsam ipsum magnam, officia quia quibusdam quidem. Cum deserunt fugit minima sequi, similique ut.\n' +
-    //     '                  '
-    // const shrRef = useRef(null)
-
-    // const click = (event) => {
-    //     console.log(event.target.attributes['data-file-name'].nodeValue)
-    //     console.log(event.target)
-    // }
+    const addPostHref = (post: IPostData) => {
+        if (!post.category.editable) {
+            return `${post.category.titleUrl}/${post.creator?.nickName}/posts/${post.titleUrl}`
+        }
+        return `${post.category.titleUrl}/posts/${post.titleUrl}`
+    }
 
   return (
     <>
@@ -28,21 +25,38 @@ export default function Home({categories}:any) {
       <main>
           <div style={{minHeight:'1000px'}}>
               <CategoryNav categories={categories}/>
-              {/*<img src={`/api/static/images/9ddca34b-35e4-4acb-914d-b394a19b02aa/9ddca34b-35e4-4acb-914d-b394a19b02aa.jpeg`} alt="123"/>*/}
+              {
+                posts?.map((post) => {
+                    return(
+                        <>
+                            <div>
+                                <Link href={addPostHref(post)}>
+                                    {post.title}
+                                </Link>
+                                <div>
+                                    {post.viewsCount}
+                                    {post.category.title}
+                                </div>
+                            </div>
 
-              {/*<div ref={shrRef} style={{width:'100%', maxWidth:'300px', height:'200px'}}>*/}
-              {/*    <ShrinkText text={ttt} wrapperRef={shrRef}/>*/}
-              {/*</div>*/}
+
+                        </>
+                    )
+                })
+              }
           </div>
       </main>
     </>
   )
 }
 
+export default Home;
+
 export async function getServerSideProps() {
     const categories = await fetch(`${API_URL}/blog/category/`).then(r => r.json())
+    const posts = await fetch(`${API_URL}/posts/`).then(r => r.json())
 
     return {
-        props: {categories: categories}
+        props: {categories: categories, posts: posts}
     }
 }
