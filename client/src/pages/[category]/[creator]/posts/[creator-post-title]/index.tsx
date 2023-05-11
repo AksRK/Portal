e71b2c3e73a77a1cf33cb1467e5235ed7b02dcfd
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-12 01:22:26 +0300

    Добавлен новый запрос на получение поста + креатора

--- client/src/pages/[category]/[creator]/posts/[creator-post-title]/index.tsx
+++ client/src/pages/[category]/[creator]/posts/[creator-post-title]/index.tsx
@@ -18,7 +18,8 @@ export default CreatorPostPage;
 export async function getServerSideProps(context:GetServerSidePropsContext ) {
 	const [currentCategory, currentCreator] = await Promise.all([
 		fetch(`${API_URL}/blog/category/url/${context.params?.category}`).then(r => r.json()),
-		fetch(`${API_URL}/blog/category/url/${context.params?.category}`).then(r => r.json())
+		fetch(`${API_URL}/creators/nick-name/${context.params?.creator}`).then((res) => res.json()),
+
 	])
 	if (currentCategory?.statusCode == 404 || currentCreator?.statusCode == 404) {
 		context.res.setHeader("Location", "/");
@@ -26,5 +27,7 @@ export async function getServerSideProps(context:GetServerSidePropsContext ) {
 		context.res.end();
 		return { props: {} };
 	}
-	return {props: {}}
+
+	const post = await fetch(`${API_URL}/posts/query/one/?categoryId=${currentCategory?._id}&creatorId=${currentCreator._id}&titleUrl=${context.params?.['creator-post-title']}`).then((res) => res.json())
+	return {props: {post: post}}
 }
