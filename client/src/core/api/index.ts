1c16ec6ae57deb6048c5c8d9ba7c537d4bddb67d
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:56:46 +0300

    создан новый инстанс axios и произведено разделение на публичные запросы и приватные

--- client/src/core/api/index.ts
+++ client/src/core/api/index.ts
@@ -1,31 +1,37 @@
 import axios from "axios";
-import {AuthResponse} from "@/core/types/response";
+import {IAuthResponse} from "@/core/types/response";
+import {API_URL} from "@/core/constants";
 
-const $api = axios.create({
-	// withCredentials: true,
+const $public_api = axios.create({
+	baseURL: '/api/'
+})
+const $public_api_from_server = axios.create({
+	baseURL: API_URL
+})
+const $private_api = axios.create({
 	baseURL: '/api/'
 })
 
-$api.interceptors.request.use(config => {
+$private_api.interceptors.request.use(config => {
 	config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
 	return config
 })
 
-$api.interceptors.response.use((config) => {
+$private_api.interceptors.response.use((config) => {
 	return config;
 },async (error) => {
 	const originalRequest = error.config;
 	if (error.response.status == 401 && error.config && !error.config._isRetry) {
 		originalRequest._isRetry = true;
 		try {
-			const response = await axios.get<AuthResponse>(`/api/auth/refresh/`, {withCredentials: true, headers:{Authorization:`Bearer ${localStorage.getItem('refreshToken')}`}})
+			const response = await axios.get<IAuthResponse>(`/api/auth/refresh/`, {withCredentials: true, headers:{Authorization:`Bearer ${localStorage.getItem('refreshToken')}`}})
 			localStorage.setItem('accessToken', response.data.accessToken);
 			localStorage.setItem('refreshToken', response.data.refreshToken );
-			return $api.request(originalRequest);
+			return $private_api.request(originalRequest);
 		} catch (e) {
 			console.log('НЕ АВТОРИЗОВАН')
 		}
 	}
 	throw error;
 })
-export default $api
+export {$public_api, $public_api_from_server, $private_api}
