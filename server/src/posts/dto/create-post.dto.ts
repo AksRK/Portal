76bb707a992b8714a9f0276a58b095f0990e9e4a
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:49:44 +0300

    Добавлена валидация данных для создания и обновления постов

--- server/src/posts/dto/create-post.dto.ts
+++ server/src/posts/dto/create-post.dto.ts
@@ -1,17 +1,20 @@
+import {IsNotEmpty, IsString, MinLength} from "class-validator";
+
 export class CreatePostDto {
+	@IsString()
+	@MinLength(6, {message: 'Title cannot be empty'})
 	title: string;
-
+	@IsNotEmpty({message:'Picture cannot be empty'})
 	mainImg: string
+	@IsString()
+	@MinLength(6, {message:'Description cannot be empty'})
 	description: string;
-
+	@IsString()
+	@MinLength(6, {message:'Post cannot be empty'})
 	content: string
-
+	@IsNotEmpty({message:'Category cannot be empty'})
 	category: string;
-
 	creator?: string
-
 	readAlso?: [string]
-
-	viewsCount?: number
 
 }
