a5326bfe0bb41a88df4957f3ef2c09a0dcabc982
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:46:21 +0300

    Добавлен запрос для поиска креатора по квери параметрам, при удалении креатора удаляется загруженное фото, + правки

--- server/src/creators/creators.controller.ts
+++ server/src/creators/creators.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, Query} from '@nestjs/common';
 import { CreatorsService } from './creators.service';
 import { CreateCreatorDto } from './dto/create-creator.dto';
 import { UpdateCreatorDto } from './dto/update-creator.dto';
+import {DEFAULT_DOCS_LIMIT, DEFAULT_PAGE_PAGINATION} from "../common/constants";
 
 @Controller('creators')
 export class CreatorsController {
@@ -13,23 +14,24 @@ export class CreatorsController {
   }
 
   @Get()
-  findAll() {
-    return this.creatorsService.findAll();
+  findAll(
+      @Query('page') page: string = DEFAULT_PAGE_PAGINATION,
+      @Query('limit') limit: string = DEFAULT_DOCS_LIMIT,
+  ) {
+    return this.creatorsService.findAll(page,limit);
   }
 
-  @Get('/admin/')
+  @Get('/admin/list')
   findAllForAdmin() {
     return this.creatorsService.findAllForAdmin();
   }
-
-  @Get(':id')
-  findOneById(@Param('id') id: string) {
-    return this.creatorsService.findOneById(id);
-  }
-
-  @Get('nick-name/:nickName')
-  findOneByNickName(@Param('nickName') nickName: string) {
-    return this.creatorsService.findOneByNickName(nickName);
+  @Get('/query/one')
+  findOne(
+      @Query('id') id?: string,
+      @Query('fullName') fullName?: string,
+      @Query('nickName') nickName?: string,
+  ) {
+    return this.creatorsService.findOneByQuery(id, fullName, nickName)
   }
 
   @Patch(':id')
--- server/src/creators/creators.service.ts
+++ server/src/creators/creators.service.ts
@@ -3,11 +3,16 @@ import { CreateCreatorDto } from './dto/create-creator.dto';
 import { UpdateCreatorDto } from './dto/update-creator.dto';
 import {Creator, CreatorDocument} from "./schemas/creator.schema";
 import {InjectModel} from "@nestjs/mongoose";
-import {Model} from "mongoose";
+import {Model, PaginateModel} from "mongoose";
+import {FilesService} from "../files/files.service";
 
 @Injectable()
 export class CreatorsService {
-  constructor(@InjectModel(Creator.name) private creatorModel: Model<CreatorDocument>) {}
+  constructor(
+      @InjectModel(Creator.name) private creatorModel: Model<CreatorDocument>,
+      @InjectModel(Creator.name) private creatorModelPaginate: PaginateModel<CreatorDocument>,
+      private  filesService: FilesService
+  ) {}
   async create(createCreatorDto: CreateCreatorDto) {
     const creatorExist = await this.creatorModel.findOne({nickName: createCreatorDto.nickName})
     if (creatorExist) {
@@ -18,32 +23,56 @@ export class CreatorsService {
     return newCreator.save()
   }
 
-  async findAll() {
-    return this.creatorModel.find().exec()
+  async findAll(page: string, limit: string) {
+    const options = {
+      page: parseInt(page),
+      limit: parseInt(limit),
+      populate: [
+        {path:'photo'}
+      ]
+    }
+    return this.creatorModelPaginate.paginate({}, options)
   }
 
   async findAllForAdmin() {
     return this.creatorModel.find().select('_id fullName nickName').exec()
+  }
+  async findOneByQuery(id: string, fullName: string, nickName: string) {
+    const params = {}
+
+    if (!id && !fullName && !nickName) throw new NotFoundException()
+
+    if (id) params['_id'] = id
+    if (fullName) params['fullName'] = fullName
+    if (nickName) params['nickName'] = nickName
+
+    const creator = await this.creatorModel.findOne(params).populate({path: 'photo', select: '_id folderName folderPath originalImgPath compressedImgPath'}).exec()
+
+    if (!creator) throw new NotFoundException()
+
+    return creator
   }
 
   async findOneById(id: string) :Promise<CreatorDocument> {
     return this.creatorModel.findById(id).populate({path: 'photo', select: '_id folderName folderPath originalImgPath compressedImgPath'}).exec();
   }
 
-  async findOneByNickName(nickName: string) :Promise<CreatorDocument> {
-    const creator = await this.creatorModel.findOne({nickName: nickName}).exec();
-    if (!creator) {
-      throw new NotFoundException(`Creator ${nickName} not found`)
-    }
-    return creator
-  }
-
   async update(id: string, updateCreatorDto: UpdateCreatorDto) {
-    console.log(updateCreatorDto)
+    if (updateCreatorDto.nickName) {
+      const creator = await this.creatorModel.findOne({nickName: updateCreatorDto.nickName})
+      if (creator) throw new BadRequestException('Nickname must be unique')
+    }
+
     return this.creatorModel.findByIdAndUpdate(id, updateCreatorDto, { new: true }).exec();
   }
 
   async remove(id: string):Promise<CreatorDocument> {
-    return this.creatorModel.findByIdAndDelete(id).exec();
+    const creator = await this.creatorModel.findById(id).exec()
+
+    if (!creator) throw new NotFoundException('Creator Not Found')
+
+    await this.filesService.removeImage({id: creator.photo})
+
+    return creator.deleteOne()
   }
 }
