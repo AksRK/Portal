0db4368f121c6dedfc1aed203e80321b390e8d2a
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 21:11:06 +0300

    Переделана анимация контент блока

--- client/src/components/UI/Content/index.tsx
+++ client/src/components/UI/Content/index.tsx
@@ -1,32 +1,92 @@
 import styles from './page-body.module.scss'
-import { forwardRef, useState} from "react";
-import { useMotionValueEvent, useScroll } from "framer-motion";
-import {ContentBodyProps} from "@/core/types";
+import {CSSProperties, forwardRef, useState} from "react";
+import {AnimatePresence, useMotionValueEvent, useScroll} from "framer-motion";
+import {IContentBodyProps} from "@/core/types";
+import useWindowSize from "@/core/hooks/UseWindowSize";
+import {CONTENT_MAX_WIDTH, FIXED_WRP_MARGIN} from "@/core/constants";
+import {motion} from "framer-motion";
+import {useRouter} from "next/router";
 
-
-const Content = forwardRef<HTMLDivElement, ContentBodyProps>(({height,children, animate}, ref) => {
-	const [scrollY, setScrollY] = useState<number>(0)
-	const { scrollYProgress } = useScroll()
-	useMotionValueEvent(scrollYProgress, "change", (latest) => {
-		setScrollY(latest)
+const Content = forwardRef<HTMLDivElement, IContentBodyProps>(({height, fixedWrpHeight,children, animate}, ref) => {
+	const router = useRouter()
+	const [scrollYState, setScrollYState] = useState<number>(0)
+	const {width} = useWindowSize()
+	const { scrollY, scrollYProgress } = useScroll()
+	useMotionValueEvent(scrollY, "change", (latest) => {
+		setScrollYState(latest)
 	})
 	const setWidth = () => {
-		if (animate) {
-			if (scrollY > 0.2 && scrollY < 0.98) {
-				return {maxWidth: '100%'}
-			}
+		const contentStyle: CSSProperties = {};
+		if (height === 'full') {
+			contentStyle['minHeight'] = `calc(130vh - ${fixedWrpHeight}px)`
+		}
 
-		}
 		if (!animate) {
-			return {margin: '50px auto'}
+			contentStyle['margin'] = `${FIXED_WRP_MARGIN}px auto`
 		}
+		if (animate && width) {
+
+			if (width >= CONTENT_MAX_WIDTH+scrollYState && width <= 1920) {
+				contentStyle['maxWidth'] = `${((CONTENT_MAX_WIDTH+scrollYState)/width)*100}%`
+			}
+			else if (width > 1920 && scrollYProgress.get() < 0.2) {
+				contentStyle['transition'] = 'max-width 0.5s'
+			}
+			else if (width > 1920 && scrollYProgress.get() > 0.2) {
+				contentStyle['maxWidth'] = `100%`
+				contentStyle['transition'] = 'max-width 0.5s'
+			}
+			else if (width <= CONTENT_MAX_WIDTH+50 ) {
+				const maxWidth = ((width+scrollYState/5 - 20)/width)*100
+				contentStyle['maxWidth'] = `${maxWidth < 100?maxWidth: 100}%`
+			}
+			else {
+				contentStyle['maxWidth'] = `100%`
+			}
+		}
+
+		return contentStyle
 	}
 
+	const variants = {
+		in: {
+			opacity: 1,
+			y: 0,
+			scale: 1,
+			transition: {
+				opacity: { duration: 0.4, delay: 0.1 },
+				y: { duration: 0.5 },
+				scale: {duration: 0}
+			}
+		},
+		out: {
+			opacity: 0,
+			y: 120,
+			scale: 1.08,
+			transition: {
+				duration: 0.2
+			}
+		}
+	};
+
+
 	return (
-		<div  className={styles.pageBody} style={setWidth()} >
+		<>
 			<div ref={ref} className={styles.pageBody__mark}></div>
-			{children}
-		</div>
+			<AnimatePresence initial={false} mode={'popLayout'}>
+				<motion.div
+					className={styles.pageBody}
+					style={setWidth()}
+					key={router.asPath}
+					variants={variants}
+					animate="in"
+					initial="out"
+					exit="out"
+				>
+					{children}
+				</motion.div>
+			</AnimatePresence>
+		</>
 	);
 });
 export default Content;
