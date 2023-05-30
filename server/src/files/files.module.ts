ee3795032c7f7ff3fafbd3dc2cdfb23e79b48ac2
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:49:16 +0300

    Подключены модули для удаления фото с постов и креаторов

--- server/src/files/files.module.ts
+++ server/src/files/files.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Image, ImageSchema} from "./schemas/image.schema";
+import {PostsModule} from "../posts/posts.module";
+import {CreatorsModule} from "../creators/creators.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Image.name, schema: ImageSchema }]),
+    forwardRef(() => PostsModule),
+    forwardRef(() => CreatorsModule)
   ],
   controllers: [FilesController],
-  providers: [FilesService]
+  providers: [FilesService],
+  exports: [FilesService]
 })
 export class FilesModule {}
