15b45d92aa9ceba4a4aa53275ae6bc54e0ae4c8b
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 21:02:18 +0300

    Pos stick заменен на фиксед с маркером отслеживания

--- client/src/components/CategoryNav/index.tsx
+++ client/src/components/CategoryNav/index.tsx
@@ -1,51 +1,92 @@
 import styles from './category-nav.module.scss'
-import {FC, Fragment} from "react";
+import {FC, useRef, Fragment, useEffect, useState} from "react";
 import NavLink from "@/components/UI/NavLink";
-import React from 'react';
-import {CategoryNavProps} from "@/core/types";
+import {ICategoryNavProps} from "@/core/types";
+import ObservedBlock from "@/components/UI/ObservedBlock";
 
-const CategoryNav:FC<CategoryNavProps> = ({categories}) => {
+const CategoryNav:FC<ICategoryNavProps> = ({categories}) => {
+	const [isSticky, setIsSticky] = useState<boolean>(false)
+	const categoryNavWrpRef = useRef<HTMLDivElement>(null)
+	const markRef = useRef<HTMLDivElement>(null)
+	const getStyle = () => {
+		let style = `${styles.categoryNavWrp} `
+		if (isSticky) {
+			style += ` ${styles.categoryNavWrp_sticky}`
+		}
+
+		return style
+	}
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (markRef.current) {
+				const componentTop = markRef.current.getBoundingClientRect().top;
+
+				if (componentTop <= 0 && !isSticky) {
+					setIsSticky(true)
+				}else {
+					setIsSticky(false)
+				}
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
 	return (
-		<div className={styles.categoryNavWrp}>
-			<div className={styles.categoryNav}>
-				{
-					categories?.map((cat)=> {
-						if (!cat.editable) {
+		<>
+			<ObservedBlock ref={markRef}/>
+			{
+				isSticky
+					?
+					<div style={{height: categoryNavWrpRef?.current?.clientHeight}}/>
+					:
+					''
+			}
+			<div ref={categoryNavWrpRef} className={getStyle()}>
+				<div className={styles.categoryNav}>
+					{
+						categories?.map((cat)=> {
+							if (!cat.editable) {
+								return (
+									<Fragment key={'react-fragment-categories-key'}>
+										<NavLink
+											key={cat._id}
+											url={'/'+cat.titleUrl}
+											className={styles.categoryNav__link}
+											activeClassName={styles.categoryNav__link_active}>
+											{cat.title}
+										</NavLink>
+										<div key={'category-separator'} className={styles.categoryNav__separator}/>
+										<NavLink
+											key={'rootCategory'}
+											url={'/'}
+											className={styles.categoryNav__link}
+											activeClassName={styles.categoryNav__link_active}
+											index={true}>
+											Все статьи
+										</NavLink>
+									</Fragment>
+								)
+							}
 							return (
-								<React.Fragment key={'react-fragment-categories-key'}>
-									<NavLink
-										key={cat._id}
-										url={'/'+cat.titleUrl}
-										className={styles.categoryNav__link}
-										activeClassName={styles.categoryNav__link_active}>
-										{cat.title}
-									</NavLink>
-									<div key={'category-separator'} className={styles.categoryNav__separator}/>
-									<NavLink
-										key={'rootCategory'}
-										url={'/'}
-										className={styles.categoryNav__link}
-										activeClassName={styles.categoryNav__link_active}
-										index={true}>
-										Все статьи
-									</NavLink>
-								</React.Fragment>
+								<NavLink
+									key={cat._id}
+									url={'/'+cat.titleUrl}
+									className={styles.categoryNav__link}
+									activeClassName={styles.categoryNav__link_active}>
+									{cat.title}
+								</NavLink>
 							)
-						}
-						return (
-							<NavLink
-								key={cat._id}
-								url={'/'+cat.titleUrl}
-								className={styles.categoryNav__link}
-								activeClassName={styles.categoryNav__link_active}>
-								{cat.title}
-							</NavLink>
-						)
-					})
-				}
+						})
+					}
+				</div>
 			</div>
-		</div>
+		</>
 
 	);
 };
