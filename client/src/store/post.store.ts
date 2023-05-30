4a370d59694939c44b4e524c7414934d85de69bc
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:54:56 +0300

    Добавлены новые методы , сохранение уже загруженных постов + загрузка новых постов

--- client/src/store/post.store.ts
+++ client/src/store/post.store.ts
@@ -1,25 +1,50 @@
-import { makeAutoObservable } from "mobx";
-
-export interface Post {
-	id: number;
-	title: string;
-	body: string;
-}
+import {makeAutoObservable, runInAction} from "mobx";
+import {IPostsLoadParams, IPostsResWithPagination} from "@/core/types";
+import PostService from "@/services/post.service";
 
 export class PostStore {
-	posts: Post[] = [];
+	postsWithPagination: IPostsResWithPagination = {
+		docs: [],
+		totalDocs: 0,
+		limit: 0,
+		totalPages: 0,
+		page: 0,
+		pagingCounter: 0,
+		hasPrevPage: false,
+		hasNextPage: false,
+		prevPage: null,
+		nextPage: null
+	};
+	isLoading: boolean = false
 
 	constructor() {
 		makeAutoObservable(this);
 	}
 
-	async loadPosts() {
-		const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-		const data = await response.json();
-		this.posts = data;
+	setLoading (bool: boolean) {
+		this.isLoading = bool
+	}
+	savePostsWithPagination(postsWithPagination: IPostsResWithPagination): void {
+		this.postsWithPagination = postsWithPagination;
 	}
 
-	addPost(post: Post) {
-		this.posts.push(post);
+	async loadPostsWithPagination({page, categoryId, creatorId}:IPostsLoadParams) {
+		this.setLoading(true)
+		try {
+			const options = {page: page, categoryId: categoryId, creatorId: creatorId}
+			const response = await PostService.getALl(options)
+			runInAction(() => {
+				this.postsWithPagination = {
+					...this.postsWithPagination,
+					...response.data,
+					docs: [...this.postsWithPagination.docs, ...response.data.docs],
+
+				};
+			});
+		}catch (e) {
+
+		}finally {
+			this.setLoading(false)
+		}
 	}
 }
