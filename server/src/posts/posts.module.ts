7f1636ac2b04b285b528f0c2343e69790d37dc13
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:51:58 +0300

    Подключен модуль для работы с файлами

--- server/src/posts/posts.module.ts
+++ server/src/posts/posts.module.ts
@@ -6,13 +6,15 @@ import {Post, PostSchema} from "./schemas/post.schema";
 import {BlogCategory, BlogCategorySchema} from "../blog-category/schemas/blog-category.schema";
 import {BlogCategoryModule} from "../blog-category/blog-category.module";
 import {CreatorsModule} from "../creators/creators.module";
+import {FilesModule} from "../files/files.module";
 
 @Module({
   imports:[
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
     MongooseModule.forFeature([{ name: BlogCategory.name, schema: BlogCategorySchema }]),
     forwardRef(() => BlogCategoryModule),
-    forwardRef(() => CreatorsModule)
+    forwardRef(() => CreatorsModule),
+    forwardRef(() => FilesModule)
   ],
   controllers: [PostsController],
   providers: [PostsService],
