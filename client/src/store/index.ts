704d8e8b675c40f0ae5bb6d53056a3b19b5cc9d9
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:55:19 +0300

    Подключены сторы для постов, креаторов, категорий

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,22 @@
 import AuthStore from "@/store/auth.store";
+import {PostStore} from "@/store/post.store";
+import {CreatorStore} from "@/store/creator.store";
+import {CategoriesStore} from "@/store/categories.store";
+import {configure} from "mobx";
 
+configure({
+
+});
 class Store {
 	authStore: AuthStore;
+	categoriesStore: CategoriesStore;
+	postStore: PostStore;
+	creatorStore: CreatorStore;
 	constructor() {
 		this.authStore = new AuthStore();
+		this.postStore = new PostStore();
+		this.creatorStore = new CreatorStore();
+		this.categoriesStore = new CategoriesStore();
 	}
 }
 const store = new Store();
