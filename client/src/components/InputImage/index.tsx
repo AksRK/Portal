ddd423ab9262ee085c0613f6fddd403f99b743b8
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 21:07:11 +0300

    Переделана загрузка картинок, отказ от формы + добавлен лоадер

--- client/src/components/InputImage/index.tsx
+++ client/src/components/InputImage/index.tsx
@@ -1,38 +1,20 @@
-import React, {useState, useCallback, FC, ChangeEvent, useEffect} from "react";
+import {useState, FC, useRef} from "react";
 import ImageService from "@/services/image.service";
 import Image from 'next/image';
 import styles from './input-image.module.scss';
-import {IconButton} from "@mui/material";
+import {CircularProgress, IconButton} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import {Alert} from "@/core/utils/alert.utils";
 import FullSizeImageModal from "@/components/FullSizeImageModal";
-import {InputImageProps} from "@/core/types";
+import {IInputImageProps} from "@/core/types";
 
-const InputImage:FC<InputImageProps> = ({imageData, setImageData}) => {
+const InputImage:FC<IInputImageProps> = ({imageData, setImageData}) => {
 	const [fullSizePreview, setFullSizePreview] = useState<boolean>(false)
-
-	const imgUpload = async (event: ChangeEvent<HTMLInputElement> | null) => {
-		const image = event?.target?.files?.[0];
-
-		const resetInput = () => {
-			event?.target?.form && event.target.form.reset();
-		}
-
-		if (image) {
-			await ImageService.upload(image)
-				.then((response) => {
-					setImageData(response.data)
-					Alert({msg: 'Картинка успешно загружена', type:'success'})
-				})
-				.catch((error) => {
-					resetInput()
-					Alert({msg: error.response.data.message, type:'info'})
-				});
-		}
-	};
-
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const ref = useRef<HTMLInputElement>(null)
 	const removeImg = async () => {
+		setIsLoading(true)
 		await ImageService.delete({id: imageData?._id})
 			.then((respone) => {
 				setImageData(null)
@@ -41,24 +23,48 @@ const InputImage:FC<InputImageProps> = ({imageData, setImageData}) => {
 			.catch((error) => {
 				Alert({msg: error.response.data.message, type:'info'})
 			})
+			.finally(()=> setIsLoading(false));
 	}
-
 	const handleFullSizePreview = () => {
 		setFullSizePreview(!fullSizePreview)
 	}
+	const handleFileInputChange = async () => {
+		if (ref.current && ref?.current?.files) {
+			const image = ref?.current?.files[0];
+
+			if (image) {
+				setIsLoading(true)
+				await ImageService.upload(image)
+					.then((response) => {
+						setImageData(response.data)
+						Alert({msg: 'Картинка успешно загружена', type:'success'})
+					})
+					.catch((error) => {
+						clearInput()
+						Alert({msg: error.response.data.message, type:'info'})
+					})
+					.finally(()=> setIsLoading(false));
+			}
+		}
+	};
+	const clearInput = () => {
+		if (ref.current) {
+			ref.current.value = '';
+		}
+	};
 
 	return (
-		<form>
+		<>
 			<div className={styles.inputImage}>
 				{
 					imageData
 						?
 						<>
 							<div className={styles.inputImage__tools}>
-								<IconButton onClick={handleFullSizePreview} aria-label="delete">
+								<IconButton disabled={isLoading} onClick={handleFullSizePreview} aria-label="delete">
 									<OpenInFullIcon />
 								</IconButton>
-								<IconButton onClick={removeImg} aria-label="delete">
+								<IconButton disabled={isLoading} onClick={removeImg} aria-label="delete">
 									<DeleteIcon />
 								</IconButton>
 							</div>
@@ -68,26 +74,30 @@ const InputImage:FC<InputImageProps> = ({imageData, setImageData}) => {
 								fill={true}
 								sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33.3vw"
 								priority={false}
-								// onError={handleImageError}
 							/>
 						</>
 						:
-						<>
+						isLoading
+							?
+							<div className={styles.inputImage__loader}>
+								<CircularProgress size={60}/>
+							</div>
+							:
 							<div className={styles.inputImage__add}>
 								<input
+									ref={ref}
 									type="file"
 									accept="image/*"
 									name={'titleImg'}
 									id={'titleImg'}
-									onChange={(event) => imgUpload(event)}
+									onChange={handleFileInputChange}
 								/>
 							</div>
-						</>
+
 				}
 			</div>
-
 			<FullSizeImageModal state={fullSizePreview} setState={setFullSizePreview} imgUrl={`/api/${imageData?.originalImgPath}`}/>
-		</form>
+		</>
 	);
 };
 
