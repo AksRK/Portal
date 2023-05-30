cd64dd65bfedd26d715da7a5fa8442b88dae8563
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 21:10:28 +0300

    Добавлена обертка для роутов связанных с админкой

--- client/src/components/RouteProvider/index.tsx
+++ client/src/components/RouteProvider/index.tsx
@@ -1,12 +1,13 @@
-import React, {FC} from 'react';
-import {RouteProviderProps} from "@/core/types";
+import {FC} from 'react';
+import {IRouteProviderProps} from "@/core/types";
 import {useRouter} from "next/router";
-import {AdminLayout, AppLayout} from "@/components/Layout";
 import {ToastContainer} from "react-toastify";
+import AppLayout from "@/components/Layout/AppLayout";
+import AdminLayout from "@/components/Layout/AdminLayout";
+import PaddingWrp from "@/components/UI/PaddingWrp";
 
-const RouteProvider:FC<RouteProviderProps> = ({children}) => {
+const RouteProvider:FC<IRouteProviderProps> = ({children}) => {
 	const router = useRouter()
-
 	if (router.asPath.includes('/admin')) {
 		return (
 			<>
@@ -15,11 +16,15 @@ const RouteProvider:FC<RouteProviderProps> = ({children}) => {
 						!router.asPath.includes('signin')
 							?
 							<AdminLayout>
-								{children}
+								<PaddingWrp size={'small'}>
+									{children}
+								</PaddingWrp>
 							</AdminLayout>
 							:
 							<>
-								{children}
+								<PaddingWrp size={'small'}>
+									{children}
+								</PaddingWrp>
 							</>
 					}
 					<ToastContainer/>
