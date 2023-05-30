86e3a3068958727312018de30b2e14a0e2a3680f
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:57:18 +0300

    Отступ и максимальная ширина контейнера вынесены в отдельные константы

--- client/src/core/constants/index.ts
+++ client/src/core/constants/index.ts
@@ -3,3 +3,6 @@ export const API_HOSTNAME = 'localhost'
 export const API_PORT = '4444'
 
 export const API_URL = API_PROTOCOL+'://'+API_HOSTNAME+':'+API_PORT
+
+export const FIXED_WRP_MARGIN = 50
+export const CONTENT_MAX_WIDTH = 1633
