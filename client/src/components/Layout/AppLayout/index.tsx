3b9100aa41b73a5cda93632c466c38c78449bbd4
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 21:09:32 +0300

    Изменена логика отслеживания блоков для корректной отрисовки хедера\футера, навигация по категориям теперь берет данные из стора

--- client/src/components/Layout/AppLayout/index.tsx
+++ client/src/components/Layout/AppLayout/index.tsx
@@ -1,70 +1,74 @@
-import {FC, useEffect, useRef, useState} from "react";
+import {FC, useContext, useEffect, useRef, useState} from "react";
 import {useInView} from "framer-motion";
 import Header from "@/components/Header";
 import FixedWrp from "@/components/FixedWrp";
 import Content from "@/components/UI/Content";
 import Footer from "@/components/Footer";
 import {useRouter} from "next/router";
-import {AppLayoutProps} from "@/core/types";
+import {IAppLayoutProps} from "@/core/types";
+import {Context} from "@/components/StoreProvider";
+import CategoryNav from "@/components/CategoryNav";
+import {observer} from "mobx-react-lite";
+import ObservedBlock from "@/components/UI/ObservedBlock";
 
 
-export const AppLayout:FC<AppLayoutProps> = ({children, contentAnimate= true}) => {
+const AppLayout:FC<IAppLayoutProps> = ({children, contentAnimate= true}) => {
+	const {store} = useContext(Context)
 	const router = useRouter()
 	const [bannerBoxHeight, setBannerBoxHeight] = useState<number>(0)
 	const [footerBoxHeight, setFooterBoxHeight] = useState<number>(0)
-	const headerWithBannerRef = useRef<any>(null)
-	const footerRef = useRef<any>(null)
-	const contentObservedBlock = useRef<any>(null)
-	const observedBlockState = useInView(contentObservedBlock)
+
+	const headRef = useRef<HTMLDivElement>(null)
+	const footerRef = useRef<HTMLDivElement>(null)
+
+	const topContentRef = useRef<HTMLDivElement>(null)
+	const observedTopContentRef = useInView(topContentRef);
+	const bottomContentRef = useRef<HTMLDivElement>(null)
+	const observedBottomContentRef = useInView(bottomContentRef)
+
+	const categoryNavAllowedRoutes = ['[creator]', '[category]']
+	const isHomeBlog = router.pathname.length > 1?categoryNavAllowedRoutes.some(item => router.pathname.includes(item)) && !router.asPath.includes('posts'):true
 
 	useEffect(()=> {
-		if (contentAnimate) {
-			setBannerBoxHeight(headerWithBannerRef.current.clientHeight)
+		if (contentAnimate && headRef.current && footerRef.current) {
+			setBannerBoxHeight(headRef.current.clientHeight)
 			setFooterBoxHeight(footerRef.current.clientHeight)
 		}
 	},[router.asPath])
-
-	if (router.asPath.includes('admin')) {
-		return (
-			<>
-				<Header/>
-				<Content ref={contentObservedBlock} height={'auto'} animate={contentAnimate}>
-					{children}
-				</Content>
-
-			</>
-		)
-	}
 
 	return (
 		<>
 			{
 				contentAnimate
 					?
-					<FixedWrp isVisible={observedBlockState} ref={headerWithBannerRef} position={'header'} height={bannerBoxHeight}>
+					<FixedWrp isVisible={observedTopContentRef} ref={headRef} position={'header'} height={bannerBoxHeight}>
 						<Header/>
+						{
+							isHomeBlog? <div style={{height:'300px'}}></div> :''
+						}
 					</FixedWrp>
 					:
 					<Header/>
 			}
-
-
-			<Content ref={contentObservedBlock} height={'auto'} animate={contentAnimate}>
+			<ObservedBlock ref={topContentRef}/>
+			<Content height={'full'} fixedWrpHeight={bannerBoxHeight} animate={contentAnimate}>
+				{
+					isHomeBlog? <CategoryNav categories={store.categoriesStore.categories}/> :''
+				}
 				{children}
 			</Content>
-
+			<ObservedBlock ref={bottomContentRef}/>
 			{
 				contentAnimate
 					?
-					<FixedWrp isVisible={!observedBlockState} ref={footerRef} position={'footer'} height={footerBoxHeight}>
+					<FixedWrp isVisible={observedBottomContentRef} ref={footerRef} position={'footer'} height={footerBoxHeight}>
 						<Footer/>
 					</FixedWrp>
 					:
 					<Footer/>
 			}
-
 		</>
 	);
 };
 
-// export default Index;
+export default observer(AppLayout);
