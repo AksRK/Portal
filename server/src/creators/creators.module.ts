58c55d6a3caec640dc39c9d8edc2bc593b362fab
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-05-30 18:47:26 +0300

    Подключен модуль для работы с файлами

--- server/src/creators/creators.module.ts
+++ server/src/creators/creators.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { CreatorsService } from './creators.service';
 import { CreatorsController } from './creators.controller';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Creator, CreatorSchema} from "./schemas/creator.schema";
+import {FilesModule} from "../files/files.module";
 
 @Module({
   imports:[
-    MongooseModule.forFeature([{ name: Creator.name, schema: CreatorSchema }])
+    MongooseModule.forFeature([{ name: Creator.name, schema: CreatorSchema }]),
+    forwardRef(() => FilesModule)
   ],
   controllers: [CreatorsController],
   providers: [CreatorsService],
